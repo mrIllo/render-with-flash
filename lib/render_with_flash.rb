98ccf5a7e641c42bf709727be52fef5d39eaa9ab
Christoph Illnar (VETMAGAZIN) <illnar@communitor.com>
2024-01-02 01:29:08 +0100

    Fixed inclusion path to work again with Rails 6.0

--- lib/render_with_flash.rb
+++ lib/render_with_flash.rb
@@ -3,5 +3,5 @@ require_relative './render_with_flash/render'
 if defined?(Rails::Railtie)
   require_relative './render_with_flash/railtie'
 else
-  AbstractController::Base.send :prepend, RenderWithFlash::Render
+  ActionController::Base.send :prepend, RenderWithFlash::Render
 end
--- lib/render_with_flash/railtie.rb
+++ lib/render_with_flash/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module RenderWithFlash
   class Railtie < ::Rails::Railtie
     initializer 'render_with_flash.add_to_action_controller' do
-      AbstractController::Base.send :prepend, RenderWithFlash::Render
+      ActionController::Base.send :prepend, RenderWithFlash::Render
     end
   end
 end
