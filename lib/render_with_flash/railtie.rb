2445718f69c88af396c40392c9ad227eb48dd6bf
Christoph Illnar (VETMAGAZIN) <illnar@communitor.com>
2024-01-08 17:55:04 +0100

    prepending to ActionController::Rendering instead of ActionController::Base

--- lib/render_with_flash/railtie.rb
+++ lib/render_with_flash/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module RenderWithFlash
   class Railtie < ::Rails::Railtie
     initializer 'render_with_flash.add_to_action_controller' do
-      ActionController::Base.send :prepend, RenderWithFlash::Render
+      ActionController::Rendering.send :prepend, RenderWithFlash::Render
     end
   end
 end
